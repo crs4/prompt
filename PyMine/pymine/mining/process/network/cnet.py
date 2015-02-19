121221a6d666d2f14694c57f5fe6b930647e6209
Mauro Del Rio <mauro@crs4.it>
2015-02-19 15:43:40 +0100

    first version of cnet and relative unittests

--- PyMine/pymine/mining/process/network/cnet.py
+++ PyMine/pymine/mining/process/network/cnet.py
@@ -2,49 +2,64 @@ from pymine.mining.process.network import Node, Network
 
 
 class Binding(object):
-    def __init__(self, node_ref, node_set, frequency=None):
-        self.node_ref = node_ref
+    def __init__(self, node, node_set, frequency=None):
+        self.node = node
         self.node_set = node_set
         self.frequency = frequency
-        self.net = self.node_ref.net
+        self.net = self.node.net
+
+
+class InputBinding(Binding):
+    pass
+
+
+class OutputBinding(Binding):
+    pass
 
 
 class CNode(Node):
-    def __init__(self, label, net, frequency=None):
-        super(CNode, self).__init__(label, net, frequency)
-        self.input_bindings = set()
-        self.output_bindings = set()
-
-
-    def add_input_bindings(self, node_set):
-        pass
-
-    def add_output_bindings(self, node_set):
-        pass
-
-
-
-    # def __init__(self, id=None, name=None, input_arcs=None, output_arcs=None, frequency=None):
-    #     super(CNode, self).__init__(id=id, name=name, input_arcs=input_arcs, output_arcs=output_arcs)
-    #     self.frequency = frequency
-    #     self.input_bindings = []
-    #     self.output_bindings = []
-    #
-    # def __str__(self):
-    #     doc = "name="+self.name+" " \
-    #     "frequency="+str(self.frequency)+" " \
-    #     "input_arcs="+str(self.input_arcs)+" " \
-    #     "output_arcs="+str(self.output_arcs)
-    #     return doc
+    def __init__(self, label, net, frequency=None, attrs={}):
+        super(CNode, self).__init__(label, net, frequency, attrs)
+        self.input_bindings = []
+        self.output_bindings = []
 
 
 class CNet(Network):
+    def __init__(self, label=None):
+        super(CNet,  self).__init__(label)
+        self._bindings = []
 
-    def get_bindings(self):
+    @property
+    def bindings(self):
+        return self._bindings
+
+    def replay_case(self, case):
         pass
 
-    def replay_sequence(self, sequence):
+    def replay_log(self, log):
         pass
 
-    def replay_sequence_set(self, sequence_set):
-        pass
+    def _add_binding(self, binding):
+        self._bindings.append(binding)
+        return binding
+
+    def add_input_binding(self, node, node_set, frequency=None):
+        for n in node_set:
+            if n not in node.input_nodes:
+                self.add_arc(n, node)
+
+        binding = self._add_binding(InputBinding(node, node_set, frequency))
+        node.input_bindings.append(binding)
+        return binding
+
+    def add_output_binding(self, node, node_set, frequency=None):
+        for n in node_set:
+            if n not in node.output_nodes:
+                self.add_arc(node, n)
+
+        binding = self._add_binding(OutputBinding(node, node_set, frequency))
+        node.output_bindings.append(binding)
+        return binding
+
+    def _create_node(self, label, frequency=None, attrs={}):
+        return CNode(label, self, frequency, attrs)
---
+++ PyMine/test/pymine/mining/process/network/cnet.py
@@ -0,0 +1,49 @@
+import unittest
+from pymine.mining.process.network.cnet import CNet, CNode, InputBinding, OutputBinding
+
+
+class CNetTestCase(unittest.TestCase):
+
+    def test_add_node(self):
+        net = CNet()
+        a = net.add_node('a')
+        self.assertTrue(isinstance(a, CNode))
+        self.assertTrue(len(a.input_bindings) == 0)
+        self.assertTrue(len(a.output_bindings) == 0)
+
+    def test_add_node_attrs(self):
+        net = CNet()
+        freq = 1
+        attrs = {'test': True}
+        a = net.add_node('a', frequency=freq, attrs=attrs)
+        self.assertTrue(a.label == 'a')
+        self.assertTrue(a.frequency == freq)
+        self.assertTrue(a.attrs == attrs )
+
+    def test_add_nodes(self):
+        net = CNet()
+        a, b = net.add_nodes('a', 'b')
+        self.assertTrue(a.label == 'a')
+        self.assertTrue(b.label == 'b')
+        self.assertTrue(set(net.nodes) == {a, b})
+
+    def test_add_bindings(self):
+        net = CNet()
+        a, b, c, d = net.add_nodes('a', 'b', 'c', 'd')
+        binding_b_c = net.add_output_binding(a, {b, c})
+        binding_b = net.add_output_binding(a, {b}, 1)
+        binding_d_b = net.add_input_binding(d, {b})
+        binding_d_c = net.add_input_binding(d, {d})
+
+        self.assertTrue(a.output_bindings == [binding_b_c, binding_b])
+        self.assertTrue(d.input_bindings == [binding_d_b, binding_d_c])
+        self.assertTrue(set(net.bindings) == {binding_b, binding_b_c, binding_d_b, binding_d_c})
+
+        self.assertTrue(isinstance(binding_b_c, OutputBinding))
+        self.assertTrue(isinstance(binding_d_c, InputBinding))
+
+        self.assertTrue(binding_b_c.node == a)
+        self.assertTrue(binding_b_c.node_set == {b, c})
+        self.assertTrue(binding_b.frequency == 1)
+
+
