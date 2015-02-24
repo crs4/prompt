3d629e911f047e0700855226f64a8d736aa8d0dd
Mauro Del Rio <mauro@crs4.it>
2015-02-24 12:23:37 +0100

    added replay_case and simple_fitness conformance checking

---
+++ PyMine/pymine/conformance/__init__.py
@@ -0,0 +1,39 @@
+def replay_case(case, net):
+    return net.replay_sequence([event.activity_name for event in case.events])
+
+
+def simple_fitness(log, net):
+    result = FitnessResult(log, net)
+    for case in log.cases:
+        result_case = replay_case(case, net)
+        result.add_replay_result(case, result_case)
+
+    return result
+
+
+class FitnessResult(object):
+    def __init__(self, log, net):
+        self.log = log
+        self.net = net
+        self.results = {}
+        self.failed_cases = []
+        self.correct_cases = []
+
+    def add_replay_result(self, case, replay_results):
+        self.results[case] = replay_results
+        result, obligations, unknown = replay_results
+        if result:
+            self.correct_cases.append(case)
+        else:
+            self.failed_cases.append(case)
+
+    @property
+    def fitness(self):
+        if self.results:
+            n_correct_cases = float(len(self.correct_cases))
+            n_failed_cases = float(len(self.failed_cases))
+            return n_correct_cases/(n_correct_cases + n_failed_cases)
+
+
+
+
---
+++ PyMine/test/pymine/conformance/__init__.py
@@ -0,0 +1,58 @@
+import unittest
+from pymine.conformance import replay_case, simple_fitness
+from pymine.mining.process.eventlog.factory import DictLogFactory
+from test.pymine.mining.process.network.test_cnet import _create_cnet
+
+
+class ConformanceTestCase(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(ConformanceTestCase, self).__init__(*args, **kwargs)
+
+        correct_log_dict = {
+            'test': [['a', 'b', 'c',  'e']]
+        }
+        wrong_log_dict = {
+            'test': [['a', 'b', 'c']]
+        }
+        half_correct_log_dict = {'test': correct_log_dict['test'] + wrong_log_dict['test']}
+
+        self.factory = DictLogFactory(correct_log_dict)
+        self.correct_log = self.factory.create_log()
+        self.wrong_log = DictLogFactory(wrong_log_dict).create_log()
+        self.half_correct_log = DictLogFactory(half_correct_log_dict).create_log()
+
+    def test_replay_case_on_cnet(self):
+        cnet, a, b, c, d, e = _create_cnet()
+        log = self.factory.create_log()
+        result, obligations, unknown = replay_case(log.cases[0], cnet)
+        self.assertTrue(result)
+        self.assertEqual(len(obligations), 0)
+
+    def test_replay_wrong_case_on_cnet(self):
+        cnet, a, b, c, d, e = _create_cnet()
+        result, obligations, unknown = replay_case(self.wrong_log.cases[0], cnet)
+        self.assertFalse(result)
+        self.assertEqual(obligations, [e])
+
+    def test_fitness_correct_log(self):
+        cnet, a, b, c, d, e = _create_cnet()
+        fitness_result = simple_fitness(self.correct_log, cnet)
+        self.assertEqual(fitness_result.fitness, 1)
+
+    def test_fitness_half_correct_log(self):
+        cnet, a, b, c, d, e = _create_cnet()
+        fitness_result = simple_fitness(self.half_correct_log, cnet)
+        self.assertEqual(fitness_result.fitness, 0.5)
+
+
+
+
+
+
+
+
+
+
+
+
