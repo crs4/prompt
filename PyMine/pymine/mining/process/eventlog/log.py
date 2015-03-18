a2875d5c84ec60eeca82e681bc4b0ff6dc1605a8
Mauro Del Rio <mauro@crs4.it>
2015-03-18 15:01:22 +0100

    added ProcessLog

--- PyMine/pymine/mining/process/eventlog/log.py
+++ PyMine/pymine/mining/process/eventlog/log.py
@@ -1,20 +1,61 @@
+from pymine.mining.process.eventlog.exceptions import InvalidProcess
+
+
+class ProcessLog(object):
+    def __init__(self, process, cases=None):
+        self._process = process
+        self._cases = []
+        if cases is not None:
+            for case in cases:
+                self.add_case(case)
+
+    @property
+    def process(self):
+        return self._process
+
+    @property
+    def cases(self):
+        return self._cases
+
+    def add_case(self, case):
+        if case.process != self.process:
+            raise InvalidProcess("cases must belong to process %s, found %s instead" % (self.process, case.process))
+
+        self._cases.append(case)
+        case.log = self  # TODO remove this assignment, I mean why a case should belong to a single log instance?
+
+
 class Log(object):
 
-    def __init__(self, cases=None):
-        self._processes = set()
-        self.cases = []
+    def __init__(self, cases=None, process_logs=()):
+        self._process_logs = {}
+        for p_log in process_logs:
+            self._process_logs[p_log.process] = p_log
+
         if cases is not None:
             for case in cases:
                 self.add_case(case)
 
     def add_case(self, case):
-        self.cases.append(case)
-        case.log = self
-        self._processes.add(case.process)
+        if case.process not in self._process_logs:
+            self._process_logs[case.process] = ProcessLog(case.process, [case])
+        else:
+            self._process_logs[case.process].add_case(case)
+
 
     @property
     def processes(self):
-        return list(self._processes)
+        return self._process_logs.keys()
+
+    @property
+    def cases(self):
+        cases = []
+        for process_log in self._process_logs.values():
+            cases.extend(process_log.cases)
+        return cases
+
+    def __getitem__(self, item):
+        return self._process_logs[item]
 
     def __eq__(self, other):
         if type(self) == type(other):
