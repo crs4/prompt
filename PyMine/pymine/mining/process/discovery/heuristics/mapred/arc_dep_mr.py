7e892cf1033f99c435fb74822c45013a13b28048
Mauro Del Rio <mauro@crs4.it>
2015-04-28 15:30:26 +0200

    mapred dependency miner writes correctly avro outputs

--- PyMine/pymine/mining/process/discovery/heuristics/mapred/arc_dep_mr.py
+++ PyMine/pymine/mining/process/discovery/heuristics/mapred/arc_dep_mr.py
@@ -1,32 +1,32 @@
 import sys
-import uuid
+from collections import defaultdict
 import os
-pymine_home = os.environ.get('PYMINE_HOME')
-sys.path.append(pymine_home)
-import collections
 import pydoop.mapreduce.api as api
 import pydoop.mapreduce.pipes as pp
+
+sys.stderr.write("%r\n" % (os.listdir(os.getcwd(),)))
 from pymine.mining.process.discovery.heuristics.dependency import DependencyMiner
 from pymine.mining.process.discovery.heuristics import Matrix
+from pydoop.avrolib import AvroContext
+from pymine.mining.process.eventlog.serializers.avro_serializer import convert_avro_dict_to_obj
 SEPARATOR = '->'
+import logging
+logger = logging.getLogger("mapred")
+
 
 class Mapper(api.Mapper):
 
     def __init__(self, context):
         super(Mapper, self).__init__(context)
         context.setStatus("initializing mapper")
-        self.process_id = uuid.uuid4()
-
-    def get_key(self, item):
-        return [item[0]]
 
     def map(self, context):
-        case = context.value
-        events_freq = Matrix()
+        case = convert_avro_dict_to_obj(context.value, 'Case')
+        events_freq = defaultdict(int)
         precede_matrix = Matrix()
         two_step_loop_freq = Matrix()
-        start_events = Matrix()
-        end_events = Matrix()
+        start_events = set()
+        end_events = set()
         long_distance_freq = Matrix()
         DependencyMiner.compute_precede_matrix_by_case(
             case, events_freq, precede_matrix, two_step_loop_freq, start_events, end_events, long_distance_freq)
@@ -35,13 +35,12 @@ class Mapper(api.Mapper):
 
         for e1 in events:
             for e2 in events:
-                context.emit(SEPARATOR.join([e1, e2]), [{
+                logger.debug("emitting  e1 %s, e2 %s")
+                context.emit(SEPARATOR.join([e1, e2]), {
                     'precede': precede_matrix[e1][e2],
                     'two_step_loop': two_step_loop_freq[e1][e2],
                     'long_distance': long_distance_freq[e1][e2],
-                    'start_node': e1,
-                    'end_node': e2
-                }])
+                })
 
 
 class Reducer(api.Reducer):
@@ -60,14 +59,16 @@ class Reducer(api.Reducer):
             'long_distance': 0,
 
         }
+
         for arc in context.values:
             for k in arc_info.keys():
                 arc_info[k] += arc[k]
 
         arc_info['start_node'] = start_node
         arc_info['end_node'] = end_node
-        context.emit(arc_id, arc_info)
+        print 'emitting arc_info', arc_info
+        context.emit(SEPARATOR.join([start_node, end_node]), arc_info)
 
 
 def __main__():
-    pp.run_task(pp.Factory(Mapper, Reducer))
+    pp.run_task(pp.Factory(Mapper, Reducer), private_encoding=True, context_class=AvroContext)
