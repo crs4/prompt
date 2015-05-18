eec5f10a22f5612a4e7dd8f79618702dc182aa4b
Mauro Del Rio <mauro@crs4.it>
2015-05-18 17:36:34 +0200

    fixed avro serialization/moving in hdfs

--- PyMine/pymine/mining/process/conformance/__init__.py
+++ PyMine/pymine/mining/process/conformance/__init__.py
@@ -1,4 +1,6 @@
 from pymine.mining.process.eventlog.log import Classifier
+
+
 def replay_case(case, net, classifier=None):
     """
     :param case: a :class:`pymine.mining.process.eventlog.Case` instance
@@ -10,7 +12,7 @@ def replay_case(case, net, classifier=None):
     return net.replay_sequence([classifier.get_event_name(event) for event in case.events])
 
 
-def simple_fitness(process_log, net, classifier=None):
+def simple_fitness(process_log, net, classifier=None, aggregate=True):
     """
     Compute the fitness on the given log and net as the fraction of case replayed successfully to the cardinality of log
 
