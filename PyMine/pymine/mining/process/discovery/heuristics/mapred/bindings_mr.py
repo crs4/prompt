f54260a3bfff91b841ae6fb258ffbfbe165f5eca
Mauro Del Rio <mauro@crs4.it>
2015-05-19 17:56:21 +0200

    optimized binding miner mapred using combiner

--- PyMine/pymine/mining/process/discovery/heuristics/mapred/bindings_mr.py
+++ PyMine/pymine/mining/process/discovery/heuristics/mapred/bindings_mr.py
@@ -1,14 +1,21 @@
 import pydoop.mapreduce.api as api
 import pydoop.mapreduce.pipes as pp
 from pymine.mining.process.discovery.heuristics.mapred.binding_miner_mr import BindingMiner
-from pymine.mining.mapred import deserialize_obj
+from pymine.mining.mapred import deserialize_obj, CaseContext
 from pymine.mining.process.discovery.heuristics import Matrix
-from pymine.mining.process.eventlog.serializers.avro_serializer import convert_avro_dict_to_obj
 from pymine.mining.process.discovery.heuristics.mapred import CLASSIFIER_FILENAME
-from pydoop.avrolib import AvroContext
 import logging
 logger = logging.getLogger("mapred")
 SEPARATOR = '__'
+
+
+class CustomCaseContext(CaseContext):
+    def emit(self, key, value):
+        if self.is_reducer():
+            node, b_type, binding_nodes = key
+            value = {'node': node, 'type': b_type, 'binding': list(binding_nodes), 'freq': value}
+
+        super(CustomCaseContext, self).emit(key, value)
 
 
 class Mapper(api.Mapper):
@@ -21,7 +28,7 @@ class Mapper(api.Mapper):
         self.classifier = deserialize_obj(context.job_conf.get(CLASSIFIER_FILENAME))
 
     def map(self, context):
-        case = convert_avro_dict_to_obj(context.value, 'Case')
+        case = context.value
         input_bindings = Matrix()
         output_bindings = Matrix()
         BindingMiner._mine_bindings_by_case(self.cnet, self.classifier, case, output_bindings, input_bindings)
@@ -29,9 +36,9 @@ class Mapper(api.Mapper):
         for b_type in [input_bindings, output_bindings]:
             for node, bindings in b_type.items():
                 for b in bindings:
-                    serialized_b = (sorted([n.label for n in b]))
+                    serialized_b = tuple(sorted([n.label for n in b]))
                     str_b_type = 'input' if b_type == input_bindings else "output"
-                    key = '__'.join([node.label, str_b_type] + serialized_b)
+                    key = (node.label, str_b_type, serialized_b)
                     value = b_type[node][b]
                     context.emit(key, value)
 
@@ -43,13 +50,10 @@ class Reducer(api.Reducer):
         context.set_status("initializing reducer")
 
     def reduce(self, context):
-        tmp = context.key.split(SEPARATOR)
-        node = tmp[0]
-        b_type = tmp[1]
-        binding_nodes = tmp[2:]
+        # node, b_type, binding_nodes = context.key
         total = sum(context.values)
-        context.emit(context.key, {'node': node, 'type': b_type, 'binding': binding_nodes, 'freq': total})
-
+        # context.emit(context.key, {'node': node, 'type': b_type, 'binding': binding_nodes, 'freq': total})
+        context.emit(context.key, total)
 
 def __main__():
-    pp.run_task(pp.Factory(Mapper, Reducer), private_encoding=True, context_class=AvroContext)
+    pp.run_task(pp.Factory(Mapper, Reducer, combiner_class=Reducer), private_encoding=True, context_class=CustomCaseContext)
