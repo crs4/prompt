f15127c8a33c5eda8224ac529b280ff5f4e88025
Mauro Del Rio <mauro@crs4.it>
2015-06-03 09:20:05 +0200

    binding miner optimezed

--- PyMine/pymine/mining/process/discovery/heuristics/mapred/binding_miner_mr.py
+++ PyMine/pymine/mining/process/discovery/heuristics/mapred/binding_miner_mr.py
@@ -2,12 +2,15 @@ from pymine.mining.process.discovery.heuristics import Matrix
 import pymine.mining.process.discovery.heuristics.binding_miner as bm
 from pymine.mining.mapred import MRLauncher, serialize_obj, CaseContext
 from pymine.mining.process.discovery.heuristics.mapred import CLASSIFIER_FILENAME
+import cPickle as pickle
+import pydoop.hdfs as hdfs
 import os
 
 
 class BindingMiner(bm.BindingMiner, MRLauncher):
     def __init__(self, log, classifier=None, n_reducers=None, d_kwargs=None):
         bm.BindingMiner.__init__(self, log, classifier)
+        n_reducers = 1 # FIXME
         MRLauncher.__init__(self, n_reducers, d_kwargs)
 
     CNET_FILENAME = 'cnet_file'
@@ -24,20 +27,28 @@ class BindingMiner(bm.BindingMiner, MRLauncher):
         output_dir_prefix = "bm_output"
 
         self.run_mapred_job(self.log,
-                            os.path.join(cwd, 'binding.avsc'),
+                            # os.path.join(cwd, 'binding.avsc'),
+                            None,
                             os.path.join(cwd, 'bindings_mr.py'),
                             "bindings_mr",
-                            output_dir_prefix
+                            output_dir_prefix,
+
                             )
 
-        for avro_obj in self.avro_outputs:
-            if avro_obj["type"] == 'input':
+        output_file = os.path.join(self.output_dir, 'part-r-00000')
+        with hdfs.open(output_file, 'r') as f:
+            pickled_string = f.read().split('\t')[1]
+            mr_matrix = pickle.loads(pickled_string)
+
+        for k, v in mr_matrix.iteritems():
+            node, type_, binding = k
+
+            node = cnet.get_node_by_label(node)
+            binding = {cnet.get_node_by_label(b) for b in binding}
+            if type_ == 'input':
                 matrix = input_bindings
             else:
                 matrix = output_bindings
-
-            node = cnet.get_node_by_label(avro_obj["node"])
-            binding = {cnet.get_node_by_label(b) for b in avro_obj["binding"]}
-            matrix[node][frozenset(binding)] = avro_obj['freq']
+            matrix[node][frozenset(binding)] = v
 
         self._populate_cnet(cnet, input_bindings, output_bindings, thr)
--- PyMine/pymine/mining/process/discovery/heuristics/mapred/bindings_mr.py
+++ PyMine/pymine/mining/process/discovery/heuristics/mapred/bindings_mr.py
@@ -4,23 +4,11 @@ from pymine.mining.process.discovery.heuristics.mapred.binding_miner_mr import B
 from pymine.mining.mapred import deserialize_obj, CaseContext
 from pymine.mining.process.discovery.heuristics import Matrix
 from pymine.mining.process.discovery.heuristics.mapred import CLASSIFIER_FILENAME
+from collections import defaultdict
+import cPickle as pickle
 import logging
 logger = logging.getLogger("mapred")
 SEPARATOR = '__'
-
-
-class CustomCaseContext(CaseContext):
-    def emit(self, key, value):
-        if self.is_reducer():
-            node, b_type, binding_nodes = key
-            value = {'node': node, 'type': b_type, 'binding': list(binding_nodes), 'freq': value}
-            import pyavroc
-            from pydoop.avrolib import AVRO_VALUE_OUTPUT_SCHEMA
-            jc = self.get_job_conf()
-            avtypes = pyavroc.create_types(jc.get(AVRO_VALUE_OUTPUT_SCHEMA))
-            value = avtypes.Binding(**value)
-
-        super(CustomCaseContext, self).emit(key, value)
 
 
 class Mapper(api.Mapper):
@@ -37,28 +25,39 @@ class Mapper(api.Mapper):
         input_bindings = Matrix()
         output_bindings = Matrix()
         BindingMiner._mine_bindings_by_case(self.cnet, self.classifier, case, output_bindings, input_bindings)
-
+        matrix = {}
         for b_type in [input_bindings, output_bindings]:
             for node, bindings in b_type.items():
                 for b in bindings:
                     serialized_b = tuple(sorted([n.label for n in b]))
                     str_b_type = 'input' if b_type == input_bindings else "output"
                     key = (node.label, str_b_type, serialized_b)
-                    value = b_type[node][b]
-                    context.emit(key, value)
+                    matrix[key] = b_type[node][b]
+
+        context.emit('', matrix)
 
 
-class Reducer(api.Reducer):
+class Combiner(api.Reducer):
 
     def __init__(self, context):
-        super(Reducer, self).__init__(context)
+        super(Combiner, self).__init__(context)
         context.set_status("initializing reducer")
+
+    def _emit(self, context, k, v):
+        context.emit(k, v)
 
     def reduce(self, context):
         # node, b_type, binding_nodes = context.key
-        total = sum(context.values)
+        matrix = defaultdict(int)
+        for m in context.values:
+            for k, v in m.iteritems():
+                matrix[k] +=v
         # context.emit(context.key, {'node': node, 'type': b_type, 'binding': binding_nodes, 'freq': total})
-        context.emit(context.key, total)
+        self._emit(context, '', matrix)
+
+class Reducer(Combiner):
+    def _emit(self, context, k, v):
+        context.emit(k, pickle.dumps(v))
 
 def __main__():
-    pp.run_task(pp.Factory(Mapper, Reducer, combiner_class=Reducer), private_encoding=True, context_class=CustomCaseContext, fast_combiner=True)
+    pp.run_task(pp.Factory(Mapper, Reducer, combiner_class=Combiner), private_encoding=True, context_class=CaseContext, fast_combiner=True)
