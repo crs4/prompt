55d8cc15d44b08029a2178a1446c549924d7a653
Mauro Del Rio <mauro@crs4.it>
2015-06-23 12:25:40 +0200

    fixed package_data in setup

--- setup.py
+++ setup.py
@@ -3,4 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name='prompt',
     packages=find_packages(exclude=['test', 'test*' 'scripts', 'scripts*'], include=['prompt','prompt.*']),
+    package_data = {'prompt': ['mining/process/eventlog/serializers/avro_serializer/schemas/*.avsc']},
     version=0.1)
