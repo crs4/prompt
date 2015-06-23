a58ca7d89231b9b646d7bbd441b511b26e0a8d8f
Mauro Del Rio <mauro@crs4.it>
2015-06-23 11:58:13 +0200

    fixed setup

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, find_packages
 
 setup(
     name='prompt',
-    packages=find_packages(exclude=['test', 'test*' 'scripts', 'scripts*'], include=['prompt.*']),
+    packages=find_packages(exclude=['test', 'test*' 'scripts', 'scripts*'], include=['prompt','prompt.*']),
     version=0.1)
